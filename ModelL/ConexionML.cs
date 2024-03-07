adb85ce110007adcdcab59b100d97863833eca54
yeison <vanegasyeison51@gmail.com>
2024-03-06 22:56:24 -0500

    first commit

---
+++ ModelL/ConexionML.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AplicacionEmpleadosNomina.ModelL
+{
+    class ConexionML
+    {
+        SqlConnection conexion;
+        private string cadenaconexion = "Data Source=YEISON-VANEGAS\\MSSQLSERVER01; Integrated Security=True;";
+
+        public SqlConnection EstablecerConexion()
+        {
+            try
+            {
+                this.conexion = new SqlConnection(this.cadenaconexion);
+                return this.conexion;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al establecer la conexión: " + ex.Message);
+                return null;
+            }
+        }
+
+        public bool ProbarConexion()
+        {
+            try
+            {
+                using (SqlConnection connection = EstablecerConexion())
+                {
+                    connection.Open();
+                    SqlCommand comando = new SqlCommand();
+                    comando.Connection = connection;
+                    comando.CommandText = "SELECT * FROM dbo.employees";
+                    comando.ExecuteNonQuery();
+                    connection.Close();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al probar la conexión: " + ex.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ ModelL/EmpleadosML.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionEmpleadosNomina.ModelL
+{
+    class EmpleadosML
+    {
+     public string birth_date { get;set }
+    
+    
+    }
+
+}
